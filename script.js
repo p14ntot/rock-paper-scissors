7fa52265990ca54fa5c7e04fe87ef1499ec2d3e7
p14ntot <p14ntot@ionio.gr>
2023-03-06 14:22:04 +0200

    add buttons

--- script.js
+++ script.js
@@ -1,76 +1,55 @@
-let player=getPlayersChoice();
-let computer=getComputerChoice();
-
-function getPlayersChoice(){
-    let answer=prompt('select between rock,paper and scissors');
-            if (answer != 'rock' && answer!='paper' && answer!='scissors'){
-                
-                console.log('answer must be rock paper or scissors');
-                let answer=prompt('select between rock,paper and scissors');
-            }
-            return answer;
+document.addEventListener('DOMContentLoaded', function() {
+    const btn1 = document.querySelector('#btn1');
+    const btn2 = document.querySelector('#btn2');
+    const btn3 = document.querySelector('#btn3');
+  
+    function getComputerChoice() {
+      const choice=['rock', 'paper', 'scissors'];
+      let randomChoice = Math.floor(Math.random() * choice.length);
+      console.log(choice[randomChoice]);
+      return choice[randomChoice];
+    }
+  
+    function play(player, computer) {
+      if (player === computer) {
+        console.log("It's a tie!");
+      } else if (player === "rock") {
+        if (computer === "scissors") {
+          console.log("Player wins!");
+        } else {
+          console.log("Computer wins!");
         }
-        
-function getComputerChoice(){
-            
-            const choice=['rock','paper','scissors'];
-            let randomChoice=Math.floor(Math.random()*choice.length);
-            console.log(choice[randomChoice]);
-            return choice[randomChoice];
+      } else if (player === "paper") {
+        if (computer === "rock") {
+          console.log("Player wins!");
+        } else {
+          console.log("Computer wins!");
         }
-
-        
-
-function play(player,computer){
-
-            
-
-            if (player==='rock' && computer==='scissors'){
-                console.log('player wins!')
-                
-            }   else if(player==='rock' && computer==='rock'){
-                    console.log('its a draw');
-            }   else if(player==='rock' && computer==='paper'){
-                    console.log('computer wins');}
-                    
-
-            if (player==='paper'){
-                if (computer==='paper'){
-                    console.log('its a draw');
-                }   else if (computer==='rock'){
-                     console.log('player wins!');
-                     
-                }   else{
-                        console.log('computer wins');
-                        
-                }
-            }
-
-            if (player==='scissors'){
-                if (computer==='paper'){
-                    console.log('player wins!');
-                    
-                }   else if(computer==='scissors'){
-                    console.log('its a draw');
-                }   else{
-                     console.log('computer wins');
-                     
-                }
-
-            }
-            
-
-
+      } else if (player === "scissors") {
+        if (computer === "paper") {
+          console.log("Player wins!");
+        } else {
+          console.log("Computer wins!");
         }
-
-
-
-function newGame(player,computer){
-    for (let i = 0; i < 5; i++){
-         player=getPlayersChoice();
-         computer=getComputerChoice();
-        game=play(player,computer);
-        
-    }}
-newGame(player,computer);
-        
+      }
+    }
+  
+    btn1.addEventListener('click', function(event) {
+      let player = 'rock';
+      let computer = getComputerChoice();
+      play(player, computer);
+    });
+  
+    btn2.addEventListener('click', function(event) {
+      let player = 'paper';
+      let computer = getComputerChoice();
+      play(player, computer);
+    });
+  
+    btn3.addEventListener('click', function(event) {
+      let player = 'scissors';
+      let computer = getComputerChoice();
+      play(player, computer);
+    });
+  });
+  
